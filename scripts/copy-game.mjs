10e7d583d124aad5c90ac8c704d600a2610ec9c2
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-06-18 19:00:40 -0400

    formatting

--- scripts/copy-game.mjs
+++ scripts/copy-game.mjs
@@ -4,7 +4,8 @@ import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const outDir = (...args) => join(__dirname, '../out/', ...args)
+const projectRoot = (...args) => join(__dirname, '..', ...args)
+const outDir = (...args) => projectRoot('out/', ...args)
 
 const pbcopy = data => {
   const proc = spawn('pbcopy')
@@ -18,9 +19,14 @@ const updateFile = readFileSync(outDir('update.js'), 'utf-8')
 
 console.log('[copy-game] Building game object...')
 const game = `{
-  init: (elevators, floors) => {${initFile}},
-  update: (deltaTime, elevators, floors) => {${updateFile}},
-}`
+  init: (elevators, floors) => {
+    ${initFile}
+  },
+  update: (deltaTime, elevators, floors) => {
+    ${updateFile}
+  },
+}
+`
 
 console.log('[copy-game] Writing to clipboard...')
 pbcopy(game)
